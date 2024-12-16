79c2199943659ea6994ea53b8e06b6b534a3634b
Abdeljalil NACEUR <contact@naceur-abdeljalil.com>
2024-12-16 15:11:07 +0400

    - Add project

---
+++ backup-examshell/venv/bin/examshell-cli.py
@@ -0,0 +1,4 @@
+#!/usr/share/examshell/venv/bin/python3
+# EASY-INSTALL-SCRIPT: 'examshell==3.0','examshell-cli.py'
+__requires__ = 'examshell==3.0'
+__import__('pkg_resources').run_script('examshell==3.0', 'examshell-cli.py')
